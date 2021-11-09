62f36e9a6cc452140ffd825155093a22d2311ca3
twygod <twygod@gmail.com>
2021-11-09 20:52:44 +0800

    fix: Change in interest rate can disable repay of loan.

--- contracts/market/FixedInterestRateModel.sol
+++ contracts/market/FixedInterestRateModel.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "../interfaces/IInterestRateModel.sol";
 
 contract FixedInterestRateModel is Ownable, IInterestRateModel {
+    uint256 public constant BORROW_RATE_MAX_MANTISSA = 0.0005e16; //Maximum borrow rate that can ever be applied (.0005% / block)
     bool public constant override isInterestRateModel = true;
     uint256 public interestRatePerBlock;
 
@@ -33,6 +34,7 @@ contract FixedInterestRateModel is Ownable, IInterestRateModel {
     }
 
     function setInterestRate(uint256 interestRatePerBlock_) external override onlyOwner {
+        require(interestRatePerBlock_ <= BORROW_RATE_MAX_MANTISSA, "borrow rate is absurdly high");
         interestRatePerBlock = interestRatePerBlock_;
         emit LogNewInterestParams(interestRatePerBlock_);
     }
