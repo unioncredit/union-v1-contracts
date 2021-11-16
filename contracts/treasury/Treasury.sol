9692c43c20fe961b9c280e49504738e302a51b00
Max Weng <maxweng@users.noreply.github.com>
2021-11-16 17:59:06 +0800

    Enable 2-step admin change (#27)
    * chg: enable 2-step admin change
    
    * chg: add zero address guard

--- contracts/treasury/Treasury.sol
+++ contracts/treasury/Treasury.sol
@@ -10,6 +10,7 @@ contract Treasury {
 
     /// @notice Reference to token to drip (immutable)
     IERC20 public immutable token;
+    address public newAdmin;
     address public admin;
 
     struct Schedule {
@@ -27,14 +28,25 @@ contract Treasury {
         _;
     }
 
+    event LogChangeAdmin(address indexed oldAdmin, address newAdmin);
+
     constructor(IERC20 token_) {
         admin = msg.sender;
         token = token_;
     }
 
-    function setAdmin(address admin_) public onlyAdmin {
-        require(admin_ != address(0), "Treasury: admin can not be zero");
-        admin = admin_;
+    function changeAdmin(address newAdmin_) external onlyAdmin {
+        require(newAdmin_ != address(0), "New admin cannot be zero");
+        newAdmin = newAdmin_;
+    }
+
+    function acceptAdmin() external {
+        require(newAdmin != address(0) && newAdmin == msg.sender, "Must be called from new admin");
+
+        emit LogChangeAdmin(admin, newAdmin);
+
+        admin = newAdmin;
+        newAdmin = address(0);
     }
 
     /**
