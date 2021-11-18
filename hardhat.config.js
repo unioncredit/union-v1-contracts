cbc272ad175a2ec9cc98e68cde5b23bf4346fbe1
twygod <twygod@gmail.com>
2021-11-18 15:24:59 +0800

    chg: deploy contract on kovan.

--- hardhat.config.js
+++ hardhat.config.js
@@ -43,7 +43,7 @@ module.exports = {
                 settings: {
                     optimizer: {
                         enabled: true,
-                        runs: 200
+                        runs: 100
                     },
                     evmVersion: "istanbul"
                 }
