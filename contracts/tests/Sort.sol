d2129bd32a6a8049ca8df7964a58462e09811de7
twygod <twygod@gmail.com>
2021-12-02 21:08:20 +0800

    chg: update sort test.!minor

--- contracts/tests/Sort.sol
+++ contracts/tests/Sort.sol
@@ -4,25 +4,31 @@ pragma solidity 0.8.4;
 import "hardhat/console.sol";
 
 contract Sort {
-    function quick(uint256[] memory data) internal pure {
+    struct LockedInfo {
+        string user;
+        uint256 amount;
+    }
+
+    function quick(LockedInfo[] memory data) public pure returns(LockedInfo[] memory) {
         if (data.length > 1) {
             quickPart(data, 0, data.length - 1);
         }
+        return data;
     }
 
     function quickPart(
-        uint256[] memory data,
+        LockedInfo[] memory data,
         uint256 low,
         uint256 high
     ) internal pure {
         if (low < high) {
-            uint256 pivotVal = data[(low + high) / 2];
+            LockedInfo memory pivotVal = data[(low + high) / 2];
 
             uint256 low1 = low;
             uint256 high1 = high;
             for (;;) {
-                while (data[low1] < pivotVal) low1++;
-                while (data[high1] > pivotVal) high1--;
+                while (data[low1].amount < pivotVal.amount) low1++;
+                while (data[high1].amount > pivotVal.amount) high1--;
                 if (low1 >= high1) break;
                 (data[low1], data[high1]) = (data[high1], data[low1]);
                 low1++;
@@ -34,25 +40,26 @@ contract Sort {
         }
     }
 
-    function insertion(uint256[] memory data) internal pure {  
+    function insertion(LockedInfo[] memory data) public pure returns(LockedInfo[] memory) {  
         uint256 length = data.length;
         int256 preIndex;
-        uint256 current;
+        LockedInfo memory current;
         for (uint256 i = 1; i < length; i++) {
             preIndex = int(i - 1);
             current = data[i];
-            while(preIndex >= 0 && data[uint256(preIndex)] > current) {
+            while(preIndex >= 0 && data[uint256(preIndex)].amount > current.amount) {
                 data[uint256(preIndex+1)] = data[uint256(preIndex)];
                 preIndex--;
             }
             data[uint256(preIndex+1)] = current;
         }
+        return data;
     }
 
-    function selection(uint256[] memory data) internal pure {
+    function selection(LockedInfo[] memory data) public pure returns(LockedInfo[] memory) {
         uint256 length = data.length;
         uint256 minIndex;
-        uint256 temp;
+        LockedInfo memory temp;
         for (uint256 i = 0; i < length - 1; i++) {
             minIndex = i;
             for (uint256 j = i + 1; j < length; j++) {
@@ -66,33 +73,149 @@ contract Sort {
         }
     }
 
-    function bubble(uint256[] memory data) internal pure {
+    function bubble(LockedInfo[] memory data) public pure returns(LockedInfo[] memory) {
         uint256 length = data.length;
         for (uint256 i = 0; i < length; i++) {
             for (uint256 j = i + 1; j < length; j++) {
-                if (data[i] > data[j]) {
-                    uint256 temp = data[j];
+                if (data[i].amount > data[j].amount) {
+                    LockedInfo memory temp = data[j];
                     data[j] = data[i];
                     data[i] = temp;
                 }
             }
         }
+        return data;
+    } 
+
+    //InPlace Merge
+    function merge(LockedInfo[] memory arr, uint256 start, uint256 mid,uint256 end) public pure returns(LockedInfo[] memory)
+{
+        uint256 start2 = mid + 1;
+    
+        // If the direct merge is already sorted
+        if (arr[mid].amount > arr[start2].amount)
+        {
+            // Two pointers to maintain start
+        // of both arrays to merge
+        while (start <= mid && start2 <= end)
+        {
+            
+            // If element 1 is in right place
+            if (arr[start].amount <= arr[start2].amount)
+            {
+                start++;
+            }
+            else
+            {
+                LockedInfo memory value = arr[start2];
+                uint256 index = start2;
+    
+                // Shift all the elements between element 1
+                // element 2, right by 1.
+                while (index != start)
+                {
+                    arr[index] = arr[index - 1];
+                    index--;
+                }
+                arr[start] = value;
+    
+                // Update all the pointers
+                start++;
+                mid++;
+                start2++;
+            }
+        }
+    }
+}
+ 
+    function mergeSort(LockedInfo[] memory arr, uint256 l, uint256 r) public pure returns(LockedInfo[] memory) {
+        if (l < r) {
+            uint256 m = l + (r - l) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+
+        return arr;
     }
 
-    function gasCost(string memory name, function(uint256[] memory data) internal fun, uint256[] memory data)
-        internal
+
+    function oddEvenSort(LockedInfo[] memory arr, uint256 n) public pure returns(LockedInfo[] memory)
+    {
+        // Initially array is unsorted
+        bool isSorted = false;
+         
+        while (!isSorted)
+        {
+            isSorted = true;
+            LockedInfo memory temp;
+   
+            // Perform Bubble sort on odd indexed element
+            for (uint256 i=1; i<=n-2; i=i+2)
+            {
+                if (arr[i].amount > arr[i+1].amount)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+   
+            // Perform Bubble sort on even indexed element
+            for (uint256 i=0; i<=n-2; i=i+2)
+            {
+                if (arr[i].amount > arr[i+1].amount)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                    isSorted = false;
+                }
+            }
+        }
+   
+        return arr;
+    }
+
+    function gasCost(string memory name, function(LockedInfo[] memory data) internal returns(LockedInfo[] memory) fun, LockedInfo[] memory data)
+        internal returns(LockedInfo[] memory arr)
     {
         uint256 u0 = gasleft();
-        fun(data);
+        arr = fun(data);
         uint256 u1 = gasleft();
         uint256 diff = u0 - u1;
         console.log("%s GasCost: %s", name, diff);
     }
 
-    function gasCostSortInsertion(uint256[] calldata data) public {
-        gasCost("quick", quick, data);
+    function gasCost2(string memory name, function(LockedInfo[] memory data, uint256 n) internal returns(LockedInfo[] memory) fun, LockedInfo[] memory data, uint256 n)
+        internal returns(LockedInfo[] memory arr)
+    {
+        uint256 u0 = gasleft();
+        arr = fun(data,n);
+        uint256 u1 = gasleft();
+        uint256 diff = u0 - u1;
+        console.log("%s GasCost: %s", name, diff);
+    }
+
+    function gasCost3(string memory name, function(LockedInfo[] memory data, uint256 l, uint256 r) internal returns(LockedInfo[] memory) fun, LockedInfo[] memory data, uint256 l, uint256 r)
+        internal returns(LockedInfo[] memory arr)
+    {
+        uint256 u0 = gasleft();
+        arr = fun(data,l,r);
+        uint256 u1 = gasleft();
+        uint256 diff = u0 - u1;
+        console.log("%s GasCost: %s", name, diff);
+    }
+
+    
+
+    function gasCostSortInsertion(LockedInfo[] memory data) public returns(LockedInfo[] memory){
+        // gasCost("quick", quick, data);
+        // gasCost("selection", selection, data);
         gasCost("insertion", insertion, data);
         gasCost("bubble", bubble, data);
-        gasCost("selection", selection, data);
+        gasCost2("oddEvenSort", oddEvenSort, data, data.length);
+        gasCost3("inPlaceMerge", mergeSort, data, 0, data.length-1);     
     }
 }
--- test/unit/testSortGasCost.js
+++ test/unit/testSortGasCost.js
@@ -12,23 +12,25 @@ describe("Sort Contract", async () => {
         const counts = [3, 5, 10, 25, 50, 75, 100];
         let arr;
         for (let i = 0; i < counts.length; i++) {
-            console.log("############");
-            console.log("element reverse order setting");
-            arr = new Array(counts[i]);
-            //element reverse order setting
-            for (let j = 0; j < arr.length; j++) {
-                arr[j] = ethers.utils.parseEther((arr.length - j).toString());
-            }
-            console.log(`params length: ${arr.length}`);
-            await sortContract.gasCostSortInsertion(arr);
             console.log("************");
-            console.log("random element setting");
             arr = new Array(counts[i]);
             for (let j = 0; j < arr.length; j++) {
-                arr[j] = ethers.utils.parseEther(parseInt(Math.random() * 100).toString());
+                arr[j] = {
+                    user: "user_" + j,
+                    amount: ethers.utils.parseEther(parseInt(Math.random() * 100).toString())
+                };
             }
             console.log(`params length: ${arr.length}`);
             await sortContract.gasCostSortInsertion(arr);
+            //res = await sortContract.oddEvenSort(arr, arr.length);
+            // let array = [];
+            // for (let i = 0; i < res.length; i++) {
+            //     array.push({
+            //         user: res[i].user,
+            //         amount: res[i].amount.toString()
+            //     });
+            // }
+            // console.log(array);
         }
     });
 });
