b6d185e6bc21d6d09feeeece9328b297c600035c
twygod <twygod@gmail.com>
2021-12-03 21:00:58 +0800

    chg: remove redundant continue statement. (#44)

--- contracts/asset/AssetManager.sol
+++ contracts/asset/AssetManager.sol
@@ -190,7 +190,6 @@ contract AssetManager is Controller, ReentrancyGuardUpgradeable, IAssetManager {
 
                 uint256 supply = moneyMarket.getSupply(token);
                 uint256 ceiling = moneyMarket.ceilingMap(token);
-                if (ceiling <= supply) continue;
                 if (supply + amount > ceiling) continue;
 
                 poolToken.safeTransferFrom(msg.sender, address(moneyMarket), amount);
