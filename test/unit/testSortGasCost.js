643d586812b10a83fa64571899a33686b96aae28
twygod <twygod@gmail.com>
2021-12-03 16:56:34 +0800

    chg: update sort test.

--- test/unit/testSortGasCost.js
+++ test/unit/testSortGasCost.js
@@ -13,6 +13,8 @@ describe("Sort Contract", async () => {
         let arr;
         for (let i = 0; i < counts.length; i++) {
             console.log("************");
+            console.log(`params length: ${counts[i]}`);
+            console.log("random");
             arr = new Array(counts[i]);
             for (let j = 0; j < arr.length; j++) {
                 arr[j] = {
@@ -20,9 +22,18 @@ describe("Sort Contract", async () => {
                     amount: ethers.utils.parseEther(parseInt(Math.random() * 100).toString())
                 };
             }
-            console.log(`params length: ${arr.length}`);
             await sortContract.gasCostSortInsertion(arr);
-            //res = await sortContract.oddEvenSort(arr, arr.length);
+            console.log("reverse");
+            arr = new Array(counts[i]);
+            for (let j = 0; j < arr.length; j++) {
+                arr[j] = {
+                    user: "user_" + j,
+                    amount: ethers.utils.parseEther(parseInt(arr.length - j).toString())
+                };
+            }
+            await sortContract.gasCostSortInsertion(arr);
+
+            // res = await sortContract.oddEvenSort(arr, arr.length);
             // let array = [];
             // for (let i = 0; i < res.length; i++) {
             //     array.push({
