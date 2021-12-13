215a5dc5edc4979eaadc2424bc9443e6aa2a85a7
Max Weng <max.m.weng@gmail.com>
2021-12-13 12:58:13 +0800

    chg: import IDai from the interface.

--- contracts/market/UDai.sol
+++ contracts/market/UDai.sol
@@ -1,20 +1,8 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.4;
 
+import "../interfaces/IDai.sol";
 import "./UToken.sol";
-
-interface IDai is IERC20Upgradeable {
-    function permit(
-        address holder,
-        address spender,
-        uint256 nonce,
-        uint256 expiry,
-        bool allowed,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
-}
 
 contract UDai is UToken {
     function repayBorrowWithPermit(
