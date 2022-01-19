608b613f6fad6a9a80f0bf899804d62e4a79cfc8
Max Weng <max.m.weng@gmail.com>
2022-01-19 16:57:07 +0800

    chg: add tests to validate results from new governor.

--- proposals/002_UpdateGovernor/testProposal.js
+++ proposals/002_UpdateGovernor/testProposal.js
@@ -14,8 +14,40 @@ require("chai").should();
 const ethUser = "0x07f0eb0c571B6cFd90d17b5de2cc51112Fb95915"; //An address with eth
 const unionUser = "0x0fb99055fcdd69b711f6076be07b386aa2718bc6"; //An address with union
 
-let governorProxy, newGovernorAddress, _userManagerAddress, timelock, unionToken;
-describe("Test Swap Governor", async () => {
+let defaultAccount, governorProxy, newGovernor, _userManagerAddress, timelock, unionToken;
+
+const voteProposal = async governor => {
+    let res;
+    const proposalId = await governor.latestProposalIds(defaultAccount.address);
+
+    const votingDelay = await governor.votingDelay();
+    await waitNBlocks(parseInt(votingDelay) + 10);
+
+    res = await governor.state(proposalId);
+    res.toString().should.eq("1");
+
+    await governor.castVote(proposalId, 1);
+    const votingPeriod = await governor.votingPeriod();
+    await waitNBlocks(parseInt(votingPeriod));
+
+    res = await governor.state(proposalId);
+    res.toString().should.eq("4");
+
+    console.log(`Queueing proposal Id: ${proposalId}`);
+
+    await governor["queue(uint256)"](proposalId);
+
+    await increaseTime(7 * 24 * 60 * 60);
+
+    res = await governor.getActions(proposalId);
+    // console.log(res.toString());
+
+    console.log(`Executing proposal Id: ${proposalId}`);
+
+    await governor["execute(uint256)"](proposalId);
+};
+
+describe("Fix Governor propose() issues", async () => {
     before(async () => {
         await network.provider.request({
             method: "hardhat_reset",
@@ -60,13 +92,14 @@ describe("Test Swap Governor", async () => {
         await unionToken.connect(unionSigner).delegate(defaultAccount.address);
         _userManagerAddress = userManagerAddress;
         // deploy new governor
-        newGovernorAddress = await deployNewGovernor();
-        console.log({newGovernorAddress});
+        const newGovernorAddress = await deployNewGovernor();
+        newGovernor = await ethers.getContractAt("UnionGovernor", newGovernorAddress);
+        console.log({newGovernorAddress: newGovernor.address});
     });
 
-    it("Propose", async () => {
+    it("Switch to new governor", async () => {
         const {targets, values, calldatas, msg} = await getProposalParams({
-            newGovernorAddress,
+            newGovernorAddress: newGovernor.address,
             timelockAddress: timelock.address,
             governorAddress: governorProxy.address
         });
@@ -75,53 +108,23 @@ describe("Test Swap Governor", async () => {
     });
 
     it("Cast vote", async () => {
-        let res;
-        const proposalId = await governorProxy.latestProposalIds(defaultAccount.address);
-
-        const votingDelay = await governorProxy.votingDelay();
-        await waitNBlocks(parseInt(votingDelay) + 10);
-
-        res = await governorProxy.state(proposalId);
-        res.toString().should.eq("1");
-
-        await governorProxy.castVote(proposalId, 1);
-        const votingPeriod = await governorProxy.votingPeriod();
-        await waitNBlocks(parseInt(votingPeriod));
-
-        res = await governorProxy.state(proposalId);
-        res.toString().should.eq("4");
-
-        console.log(`Queueing proposal Id: ${proposalId}`);
-
-        await governorProxy["queue(uint256)"](proposalId);
-
-        await increaseTime(7 * 24 * 60 * 60);
-
-        res = await governorProxy.getActions(proposalId);
-        // console.log(res.toString());
-
-        console.log(`Executing proposal Id: ${proposalId}`);
-
-        await governorProxy["execute(uint256)"](proposalId);
+        await voteProposal(governorProxy);
     });
 
-    it("Validate results", async () => {
-        (await timelock.hasRole(ADMIN_ROLE, newGovernorAddress)).should.eq(true);
-        (await timelock.hasRole(PROPOSER_ROLE, newGovernorAddress)).should.eq(true);
-        (await timelock.hasRole(EXECUTOR_ROLE, newGovernorAddress)).should.eq(true);
+    it("Make sure new governor replaced the old one", async () => {
+        (await timelock.hasRole(ADMIN_ROLE, newGovernor.address)).should.eq(true);
+        (await timelock.hasRole(PROPOSER_ROLE, newGovernor.address)).should.eq(true);
+        (await timelock.hasRole(EXECUTOR_ROLE, newGovernor.address)).should.eq(true);
         (await timelock.hasRole(ADMIN_ROLE, governorProxy.address)).should.eq(false);
         (await timelock.hasRole(PROPOSER_ROLE, governorProxy.address)).should.eq(false);
         (await timelock.hasRole(EXECUTOR_ROLE, governorProxy.address)).should.eq(false);
     });
 
-    it("Vote with new govnernor", async () => {
+    it("Test propose() fix on the new govnernor", async () => {
         const {targets, values, sigs, calldatas, msg} = await getNewGovernorProposalParams({
             userManagerAddress: _userManagerAddress
         });
 
-        const newGovernor = await ethers.getContractAt("UnionGovernor", newGovernorAddress);
-
-        console.log({newGovernor: newGovernor.address});
         // Test Compound compatible propose() function
         await newGovernor["propose(address[],uint256[],string[],bytes[],string)"](
             targets,
@@ -130,5 +133,15 @@ describe("Test Swap Governor", async () => {
             calldatas,
             msg
         );
+    });
+
+    it("Vote on new governor", async () => {
+        await voteProposal(newGovernor);
+    });
+
+    it("Validate results from new governor", async () => {
+        const userManager = await ethers.getContractAt("UserManager", _userManagerAddress);
+        console.log({userManager: userManager.address});
+        (await userManager.newMemberFee()).should.eq(ethers.utils.parseUnits("0.1"));
     });
 });
