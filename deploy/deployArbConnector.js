b20f4cb89dd4b02bb8b1a9ac8a99e4b8129a7cd8
twygod <twygod@gmail.com>
2022-01-30 15:47:44 +0800

    new: arbconnect add deploy script.

---
+++ deploy/deployArbConnector.js
@@ -0,0 +1,29 @@
+const configs = require("../deployConfig.js");
+
+module.exports = async ({getNamedAccounts, deployments}) => {
+    const {deploy} = deployments;
+    const {deployer} = await getNamedAccounts();
+
+    const unionToken = await deployments.get("UnionToken");
+
+    if (
+        configs[chainId].ArbConnector &&
+        configs[chainId].ArbConnector.l1GatewayRouter &&
+        configs[chainId].ArbConnector.destinationAddress
+    ) {
+        await deploy("ArbConnector", {
+            from: deployer,
+            args: [
+                unionToken.address,
+                configs[chainId].ArbConnector.l1GatewayRouter,
+                configs[chainId].ArbConnector.destinationAddress
+            ],
+            log: true
+        });
+
+        const arbConnector = await deployments.get("ArbConnector");
+        await arbConnector.approveToken();
+    }
+};
+module.exports.tags = ["ArbConnector"];
+module.exports.dependencies = ["UnionToken"];
