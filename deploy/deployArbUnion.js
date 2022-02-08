ee9006e963e965f812638ec9b3335d6a8ff1f62f
Min Weng (Max) <max.m.weng@gmail.com>
2022-02-08 13:47:08 +0800

    fix: deploy tests

--- deploy/deployArbUnion.js
+++ deploy/deployArbUnion.js
@@ -1,14 +1,17 @@
+const {network} = require("@openzeppelin/cli");
 const configs = require("../deployConfig.js");
 
 module.exports = async ({getNamedAccounts, deployments}) => {
-    const {deploy} = deployments;
-    const {deployer} = await getNamedAccounts();
-    const chainId = await getChainId();
+    if (network.name === "arbitrumRinkeby") {
+        const {deploy} = deployments;
+        const {deployer} = await getNamedAccounts();
+        const chainId = await getChainId();
 
-    await deploy("ArbUnion", {
-        from: deployer,
-        args: [configs[chainId]["ArbL2Gateway"], configs[chainId]["ArbUnionWrapper"]],
-        log: true
-    });
+        await deploy("ArbUnion", {
+            from: deployer,
+            args: [configs[chainId]["ArbL2Gateway"], configs[chainId]["ArbUnionWrapper"]],
+            log: true
+        });
+    }
 };
 module.exports.tags = ["ArbUnion", "Arbitrum"];
