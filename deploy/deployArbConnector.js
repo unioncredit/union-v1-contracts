78f1d1edfe1996056d51e59635c35a0485e1f038
twygod <twygod@gmail.com>
2022-02-09 20:34:30 +0800

    chg: update deploy script.!minor

--- deploy/deployArbConnector.js
+++ deploy/deployArbConnector.js
@@ -1,25 +1,27 @@
 const configs = require("../deployConfig.js");
 
 module.exports = async ({getNamedAccounts, deployments, getChainId}) => {
-    const {deploy, execute} = deployments;
-    const {deployer} = await getNamedAccounts();
-    const chainId = await getChainId();
-    const unionToken = await deployments.get("UnionToken");
-    const unionWrapper = await deployments.get("ArbUnionWrapper");
+    if (network.name === "rinkeby" || network.name === "mainnet") {
+        const {deploy, execute} = deployments;
+        const {deployer} = await getNamedAccounts();
+        const chainId = await getChainId();
+        const unionToken = await deployments.get("UnionToken");
+        const unionWrapper = await deployments.get("ArbUnionWrapper");
 
-    if (configs[chainId].ArbConnector && configs[chainId].ArbConnector.destinationAddress) {
-        await deploy("ArbConnector", {
-            from: deployer,
-            args: [unionToken.address, unionWrapper.address, configs[chainId].ArbConnector.destinationAddress],
-            log: true
-        });
+        if (configs[chainId].ArbConnector && configs[chainId].ArbConnector.destinationAddress) {
+            await deploy("ArbConnector", {
+                from: deployer,
+                args: [unionToken.address, unionWrapper.address, configs[chainId].ArbConnector.destinationAddress],
+                log: true
+            });
 
-        tx = await execute("ArbConnector", {from: deployer}, "approveToken");
-        console.log("ArbConnector approve, tx is:", tx.transactionHash);
+            tx = await execute("ArbConnector", {from: deployer}, "approveToken");
+            console.log("ArbConnector approve, tx is:", tx.transactionHash);
 
-        const admin = configs[chainId]["Admin"];
-        tx = await execute("ArbConnector", {from: deployer}, "transferOwnership", admin);
-        console.log("ArbConnector transferOwnership, tx is:", tx.transactionHash);
+            const admin = configs[chainId]["Admin"];
+            tx = await execute("ArbConnector", {from: deployer}, "transferOwnership", admin);
+            console.log("ArbConnector transferOwnership, tx is:", tx.transactionHash);
+        }
     }
 };
 module.exports.tags = ["ArbConnector"];
