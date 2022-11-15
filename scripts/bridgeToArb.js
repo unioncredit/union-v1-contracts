172003b053b343f2b655ce408060199f67642998
twygod <twygod@gmail.com>
2022-11-15 22:54:45 +0800

    chg: update bridge script.

--- scripts/bridgeToArb.js
+++ scripts/bridgeToArb.js
@@ -1,62 +1,48 @@
-const {providers, Wallet} = require("ethers");
+const {providers} = require("ethers");
 const hre = require("hardhat");
-const {ethers, getChainId} = hre;
-const {Bridge} = require("arb-ts");
+const {ethers} = hre;
+const {L1ToL2MessageGasEstimator} = require("@arbitrum/sdk/dist/lib/message/L1ToL2MessageGasEstimator");
 const {hexDataLength} = require("@ethersproject/bytes");
-
-const configs = require("../deployConfig.js");
-
-const walletPrivateKey = process.env.PRIVATE_KEY;
 
 const encodeParameters = (types, values) => {
     const abi = new ethers.utils.AbiCoder();
     return abi.encode(types, values);
 };
 
+//mainnet
+const CONNECTOR_ADDR = "0x307ED81138cA91637E432DbaBaC6E3A42699032a";
+
 const main = async () => {
-    const chainId = await getChainId();
-    let l1Provider, l2Provider, ArbUnion;
-    if (chainId == 1) {
-        ArbUnion = configs[chainId]["ArbUnion"];
-        l1Provider = new providers.JsonRpcProvider("https://mainnet.infura.io/v3/" + process.env.INFURA_ID);
-        l2Provider = new providers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
-    } else if (chainId == 4) {
-        ArbUnion = configs[chainId]["ArbUnion"];
-        l1Provider = new providers.JsonRpcProvider("https://rinkeby.infura.io/v3/" + process.env.INFURA_ID);
-        l2Provider = new providers.JsonRpcProvider("https://rinkeby.arbitrum.io/rpc");
-    } else {
-        throw new Error("network not support");
-    }
-    const l1Wallet = new Wallet(walletPrivateKey, l1Provider);
-    const l2Wallet = new Wallet(walletPrivateKey, l2Provider);
-
-    const bridge = await Bridge.init(l1Wallet, l2Wallet);
-
+    let l1Provider, l2Provider;
+    l1Provider = new providers.JsonRpcProvider("https://eth-mainnet.alchemyapi.io/v2/" + process.env.ALCHEMY_API_KEY);
+    l2Provider = new providers.JsonRpcProvider("https://arb1.arbitrum.io/rpc");
     const dripToComptrollerBytes = encodeParameters(["uint256"], ["800000000000000000"]);
     const dripToComptrollerBytesLength = hexDataLength(dripToComptrollerBytes) + 4;
-
-    const [_submissionPriceWei] = await bridge.l2Bridge.getTxnSubmissionPrice(dripToComptrollerBytesLength);
-
+    const l1ToL2MessageGasEstimate = new L1ToL2MessageGasEstimator(l2Provider);
+    console.log(`dripToComptrollerBytesLength:${dripToComptrollerBytesLength}`);
+    const _submissionPriceWei = await l1ToL2MessageGasEstimate.estimateSubmissionFee(
+        l1Provider,
+        await l1Provider.getGasPrice(),
+        dripToComptrollerBytesLength
+    );
+    console.log(`_submissionPriceWei:${_submissionPriceWei}`);
     const submissionPriceWei = _submissionPriceWei.mul(5);
     const maxGas = 275000;
-    let gasPriceBid = await bridge.l2Provider.getGasPrice();
-    //gasPriceBid = gasPriceBid.mul(ethers.BigNumber.from("2"));
+    const gasPriceBid = await l2Provider.getGasPrice();
     const callValue = submissionPriceWei.add(gasPriceBid.mul(maxGas));
 
     console.log({
         gasPriceBid: gasPriceBid.toString(),
         submissionPriceWei: submissionPriceWei.toString(),
-        callValue: callValue.toString() //ethers.utils.formatUnits(callValue)
+        callValue: callValue.toString()
     });
 
-    const connector = await ethers.getContract("ArbConnector");
-    console.log({ArbConnector: connector.address});
-
-    const tx = await connector.bridge(maxGas, gasPriceBid, submissionPriceWei, {
+    const connector = await ethers.getContract(CONNECTOR_ADDR, CONNECTOR_ABI, signer);
+    const bridgeTx = await connector.bridge(maxGas, gasPriceBid, submissionPriceWei, {
         value: callValue
     });
 
-    console.log(`Send Union to Arbitrum succeeded! 🙌 ${(await tx.wait()).transactionHash}`);
+    console.log(`Send Union to Arbitrum succeeded! 🙌 ${(await bridgeTx.wait()).transactionHash}`);
 };
 
 main()
@@ -65,3 +51,29 @@ main()
         console.error(error);
         process.exit(1);
     });
+
+const CONNECTOR_ABI = [
+    {
+        inputs: [
+            {
+                internalType: "uint256",
+                name: "maxGas",
+                type: "uint256"
+            },
+            {
+                internalType: "uint256",
+                name: "gasPriceBid",
+                type: "uint256"
+            },
+            {
+                internalType: "uint256",
+                name: "maxSubmissionCost",
+                type: "uint256"
+            }
+        ],
+        name: "bridge",
+        outputs: [],
+        stateMutability: "payable",
+        type: "function"
+    }
+];
